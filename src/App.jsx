786411665fcb2b6b3d3c38d3e51d7a11ef1489a5
Jônatas Fernandes <jonatas.tecnico19@gmail.com>
2022-12-11 22:52:16 -0300

    estruturando sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { useState } from 'react'
 import './App.css'
 
+import { SideBar } from './components/SideBar'
+
 function App() {
 
   return (
     <div className="App">
+      <SideBar />
       <p>Aqui</p>
     </div>
   )
---
+++ src/components/SideBar.jsx
@@ -0,0 +1,20 @@
+import styles from './SideBar.module.css'
+
+export function SideBar() {
+  return (
+    <div>
+      <div className="logoImage">
+        <img src="" alt="" />
+      </div>
+
+      <div className="menu">
+        <ul>
+          <li><img src="" alt="" /></li>
+          <li><img src="" alt="" /></li>
+          <li><img src="" alt="" /></li>
+          <li><img src="" alt="" /></li>
+        </ul>
+      </div>
+    </div>
+  )
+}
