f3fbfd4387c7ca4eca80ea64339241de04e177d2
Jônatas Fernandes <jonatas.tecnico19@gmail.com>
2022-12-19 17:18:59 -0300

    layout sideBar

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,18 +1,19 @@
 import styles from './SideBar.module.css'
+import { DiceFive, FileText, FileArrowUp, Planet } from "phosphor-react"
 
 export function SideBar() {
   return (
-    <div>
-      <div className="logoImage">
-        <img src="" alt="" />
+    <div className={styles.sideBar}>
+      <div className={styles.logoImage}>
+        <Planet size={50} />
+        <span>Sorteador Web</span>
       </div>
 
-      <div className="menu">
-        <ul>
-          <li><img src="" alt="" /></li>
-          <li><img src="" alt="" /></li>
-          <li><img src="" alt="" /></li>
-          <li><img src="" alt="" /></li>
+      <div className={styles.menu}>
+        <ul className={styles.funcoes}>
+          <li ><a href="#" title="Sortear Números"><DiceFive size={50} /></a></li>
+          <li><a href="#" title="Sortear Nomes"><FileText size={46} /></a></li>
+          <li><a href="#" title="Importar Lista"><FileArrowUp size={46} /></a></li>
         </ul>
       </div>
     </div>
