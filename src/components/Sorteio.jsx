3b0a5d707832974c7f0c9c5e83b4a3086e922027
Jônatas Fernandes <jonatas.tecnico19@gmail.com>
2022-12-22 11:29:25 -0300

    toggle checkbox

--- src/components/Sorteio.jsx
+++ src/components/Sorteio.jsx
@@ -3,9 +3,21 @@ import styles from './Sorteio.module.css'
 export function Sorteio() {
   return (
     <div className={styles.sorteio}>
-      <p>Defina os parâmetros</p>
+      <h3>Defina os parâmetros</h3>
       <div className={styles.parametros}>
-        <div className={styles.parametrosNumericos}></div>
+        <div className={styles.parametrosNumericos}>
+          <h2>Sortear <input type="number" name="nQuantidade" id="nQuantidade" /> números</h2>
+          <h2>entre <input type="number" name="nInicial" id="nInicial" /> e <input type="number" name="nFinal" id="nFinal" /></h2>
+        </div>
+
+        <div>
+          <label htmlFor="checkbox" className={styles.toggler}>
+            <input type="checkbox" id="checkbox" />
+            <span className={styles.ball}></span>
+            <i className={styles.sun}></i>
+            <i className={styles.moon}></i>
+          </label>
+        </div>
       </div>
     </div>
   )
